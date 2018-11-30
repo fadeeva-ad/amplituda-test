d461dfe0ba95082a34676b20933d19bb77ac91e0
Fadeeva <fadeevaad1@gmail.com>
2018-11-30 21:39:09 +0300

    added pages for gallerry and products blocks

--- dev/js/sliders.js
+++ dev/js/sliders.js
@@ -3,32 +3,32 @@ var swiper = new Swiper('.main-slider__wrapper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  breakpoints: {
-    // when window width is <= 320px
-    320: {
-      slidesPerView: auto,
-      spaceBetween: 30
-    },
-    // when window width is <= 480px
-    480: {
-      slidesPerView: 1,
-      spaceBetween: 30
-    },
-    // when window width is <= 640px
-    768: {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      navigation: false,
-      autoplay: {
-        delay: 3000,
-      },
-    },
-    1024: {
-      slidesPerView: 1,
-      spaceBetween: 30,
-      autoplay: {
-        delay: 3000,
-      },
-    }
-  }
+  // breakpoints: {
+  //   // when window width is <= 320px
+  //   320: {
+  //     slidesPerView: auto,
+  //     spaceBetween: 30
+  //   },
+  //   // when window width is <= 480px
+  //   480: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 30
+  //   },
+  //   // when window width is <= 640px
+  //   768: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 30,
+  //     navigation: false,
+  //     autoplay: {
+  //       delay: 3000,
+  //     },
+  //   },
+  //   1024: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 30,
+  //     autoplay: {
+  //       delay: 3000,
+  //     },
+  //   }
+  // }
 });
